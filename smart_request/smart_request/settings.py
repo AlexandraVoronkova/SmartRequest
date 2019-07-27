45e3e18cd066248b415e62e1ddcf08eacf0d747e
PanchenkoMaks <panchenko.maks@gmail.com>
2019-07-27 18:53:24 +0300

    merge

--- smart_request/smart_request/settings.py
+++ smart_request/smart_request/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'smart_req'
+    'smart_req',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
