d5772275694cc186f49672f69563c1bae49c7383
PanchenkoMaks <panchenko.maks@gmail.com>
2019-07-27 23:21:24 +0300

    Update models.py

--- smart_request/smart_req/models.py
+++ smart_request/smart_req/models.py
@@ -4,19 +4,25 @@ from django.contrib.auth.models import User
 
 class UserReq(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    surname = models.CharField(u"Фамилия", max_length=200, default="")
     name = models.CharField(u"Имя", max_length=200, default="")
-    patronymic = models.CharField(u"Отчество", max_length=200, default="")
     email = models.CharField(u"Почта", max_length=200, default="")
     phone = models.CharField(u"Номер телефона", max_length=200, default="")
-    address = models.CharField(u"Адрес", max_length=200, default="")
-
+    surname = models.CharField(u"Фамилия",max_length=200, default="")
+    name = models.CharField(u"Имя",max_length=200, default="")
+    patronymic = models.CharField(u"Отчество",max_length=200, default="")
+    email = models.CharField(u"Почта",max_length=200, default="")
+    phone = models.CharField(u"Номер телефона",max_length=200, default="")
+    address = models.CharField(u"Адрес",max_length=200, default="")
     def __str__(self):
+<<<<<<< HEAD
         return self.surname + " " + self.name + " " + self.patronymic
+=======
+        return self.lname+ " " +self.fname+ " " + self.sname
+>>>>>>> 97c31a6cd12b5c8efb4fc8976dafa21c81880ceb
 
 
 class ManagingOrganization(models.Model):
-    _id = models.IntegerField(u"ID Управляющей организации на Портале", null=True, blank=True)
+    id = models.IntegerField(u"ID Управляющей организации на Портале", null=True, blank=True)
     subject_rf = models.CharField(u"Субъект РФ", max_length=200, default="")
     name_full = models.CharField(u"Фирменное наименование юр лица", max_length=200, default="")
     name_short = models.CharField(u"Сокращенное наименование", max_length=200, default="")
@@ -24,8 +30,7 @@ class ManagingOrganization(models.Model):
     inn = models.CharField(u"ИНН", max_length=200, default="")
     ogrn = models.CharField(u"ОГРН", max_length=200, default="")
     legal_address = models.CharField(u"Адрес юридического лица", max_length=200, default="")
-    actual_address = models.CharField(u"Адрес фактического местонахождения органов управления", max_length=200,
-                                      default="")
+    actual_address = models.CharField(u"Адрес фактического местонахождения органов управления", max_length=200, default="")
     phone = models.CharField(u"Адрес фактического местонахождения органов управления", max_length=200, default="")
     actual_address = models.CharField(u"Телефон", max_length=200, default="")
     fax = models.CharField(u"Факс", max_length=200, default="")
@@ -37,7 +42,7 @@ class ManagingOrganization(models.Model):
 
 
 class SubjectStructure(models.Model):
-    _id = models.IntegerField(u"ID дома на Портале", null=True, blank=True)
+    id = models.IntegerField(u"ID дома на Портале", null=True, blank=True)
     region_id = models.IntegerField(u"Субъект РФ (код ФИАС)", null=True, blank=True)
     area_id = models.IntegerField(u"Район (код ФИАС)", null=True, blank=True)
     city_id = models.IntegerField(u"Населенный пункт (код ФИАС)", null=True, blank=True)
@@ -56,14 +61,13 @@ class SubjectStructure(models.Model):
     letter = models.CharField(u"Литера", max_length=200, default="")
     address = models.CharField(u"Адрес дома", max_length=200, default="")
     houseguid = models.IntegerField(u"Глобальный уникальный идентификатор дома", null=True, blank=True)
-    management_organization_id = models.ForeignKey(ManagingOrganization,
-                                                   verbose_name=u"ID Управляющей организации на Портале", blank=True,
-                                                   null=True, on_delete=models.CASCADE)
+    management_organization_id = models.ForeignKey(ManagingOrganization, verbose_name=u"ID Управляющей организации на Портале", blank=True, null=True, on_delete=models.CASCADE)
     built_year = models.IntegerField(u"Год постройки", null=True, blank=True)
     exploitation_start_year = models.IntegerField(u"Год ввода в эксплуатацию", null=True, blank=True)
     project_type = models.CharField(u"Серия, тип постройки здания", max_length=200, default="")
     house_type = models.CharField(u"Тип дома", max_length=200, default="")
     is_alarm = models.NullBooleanField(u'Факт признания дома аварийным', blank=True)
+<<<<<<< HEAD
 
 
 class CategoryReq(models.Model):
@@ -84,3 +88,5 @@ class Request(models.Model):
     status = models.CharField(default=1, max_length=200, choices=STATUS_CHOICES, null=True, blank=True)
     text = models.CharField(u"Текст запроса", max_length=1000, default="")
     date_req = models.DateField(u"Время последнего изменения заявки", null=True, blank=True, auto_now_add=True)
+=======
+>>>>>>> 97c31a6cd12b5c8efb4fc8976dafa21c81880ceb
